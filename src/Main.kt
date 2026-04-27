9c2561b766132a93922e13dbb8052bb30b7e667f
SrAndres6 <andresrjramillo671@gmail.com>
2026-04-27 13:02:21 -0500

    Refactor main function and clean up comments
    Removed tutorial comments and updated main function to execute multiple blocks with a structured output.

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import ejercicios.*
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+fun main() {
+    println("========================================")
+    println("   INICIANDO TALLER DE KOTLIN")
+    println("========================================")
+    
+    ejecutarBloque1()
+    ejecutarBloque2()
+    ejecutarBloque3()
+    ejecutarBloque4()
+    ejecutarBloque5()
+    ejecutarBloque6()
+    
+    println("\n========================================")
+    println("       FIN DEL TALLER - 30 EJERCICIOS")
+    println("========================================")
 }
